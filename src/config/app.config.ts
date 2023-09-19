22e65f1f0a713c8c42dca4dc3ed08ef9422c9e7e
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-09-19 16:39:20 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: [],
+  tenantRoles: ['Owner', 'Admin'],
+  tenantName: 'Company',
+  applicationName: 'Construction tool rental v1',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [],
+  ownerAbilities: ['Manage users', 'Manage companies', 'Manage tools', 'Manage rental requests'],
+  getQuoteUrl: 'https://app.roq.ai/proposal/7066e527-c2f0-4ec7-a218-4d5e2f176cd5',
+};
